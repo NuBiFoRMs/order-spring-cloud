bcb0c6786f151f375746eecc0edab3b0186416a6
NuBiFoRM <nubiform@me.com>
2021-07-26 22:07:28 +0900

    catalog service

---
+++ catalog-service/src/main/java/com/nubiform/catalogservice/entity/CatalogEntity.java
@@ -0,0 +1,34 @@
+package com.nubiform.catalogservice.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "catalogs")
+public class CatalogEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+}
